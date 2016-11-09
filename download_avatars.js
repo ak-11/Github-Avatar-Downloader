469dd6e713cd450630102f15e76e702b3f89f3ae
Andrew Kim <your@email.com>
2016-11-09 12:58:02 -0500

    Corrected mistakes in download_avatars.js, added example of my config file, and installed npm.

--- download_avatars.js
+++ download_avatars.js
@@ -1,69 +1,44 @@
 "use strict";
 const config = require ("./config")
 const request = require("request");
-const http = require("https");
 const fs = require("fs");
 
 const GITHUB_USER = config["username"];
 const GITHUB_TOKEN = config["token"];
 
-console.log("Welcome to the Github Avatar Downloader!");
+const owner = process.argv[2];
+const repo = process.argv[3];
 
-function getRepoContributors(repoOwner, repoName, cb) {
+const getRepoContributors = function (repoOwner, repoName, cb) {
   const requestURL = `https:// ${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors`;
 
   request.get({
     url: requestURL,
-    headers: {
-      "User-Agent": "Me"
+  }, function (error, response, body) {
+    if (error)  {
+      throw error;
     }
-  },
-  function (error, response, body) {
-    if (!error && response.statusCode == 200) {
+    if (response.statusCode === 200)  {
       const data = JSON.parse(body)
       cb(data);
-    } else {
-      console.log(error);
-      console.log(response);
     }
   });
 };
 
-function downloadImageByURL(url, filepath)  {
+const downloadImageByURL = function (url, filepath)  {
   request.get(url)
-     .on("error", function (err) {
-     console.log(err);
-    })
-    .on("response", function (response) {
-     console.log(response.statusMessage, response.headers["content-type"]);
-    })
-    .pipe(fs.createWriteStream(filepath));
+     .on("error", function (error, data) {
+       throw error;
+     })
+     .pipe(fs.createWriteStream(filepath));
 };
 
-let owner = process.argv[2];
-let repo = process.argv[3];
-// const ownerRepo = process.argv.slice(-2);
+console.log("Welcome to the Github Avatar Downloader!");
+
 getRepoContributors(owner, repo, function (url) {
   url.forEach(function (element) {
-    let url = element.avatar_url;
-    let filepath = `./avatars/${element.login}.jpg`;
-
-    console.log(url, filepath);
-    downloadImageByURL(url, filepath);
-    console.log(element["avatar_url"]);
+    const avatar_url = element.avatar_url;
+    const filepath = `./avatars/${element.login}.jpg`;
+    downloadImageByURL(avatar_url, filepath);
   })
 });
-
-
-
-// getRepoContributors("jquery", "jquery", function (url) {
-//   url.forEach(function (element) {
-//     let url = element.avatar_url
-//     let filepath = `./avatars/${element.login}.jpg`
-
-//     console.log(url, filepath)
-//     downloadImageByURL(url, filepath)
-//     console.log(element["avatar_url"]);
-//   })
-// })
-
